155f34802c47d2b25669021c05f87435a8a255d3
Leonardotheblack <leonardo_lucio_99@hotmail.com>
2021-06-28 21:51:22 -0300

    Commit

---
+++ src/componets/Gift.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="gift">
+      <div class="gift-tie-1"></div>
+      <div class="gift-tie-2"></div>
+      <div class="gift-top"></div>
+      <div class="gift-body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Gift'
+}
+</script>
+
+<style>
+.gift{
+    position: relative;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 10;
+}
+
+.gift-top {
+    width: 100px;
+    height: 25px;
+    background-color: rgb(0, 0, 0);
+}
+.gift-body {
+    width: 90px;
+    height: 60px;
+    background-color: #fff;
+}
+
+.gift-tie-1 {
+    position: absolute;
+    width: 15px;
+    height: 85px;
+    background-color: #000;
+}
+.gift-tie-2{
+    position: absolute;
+    top:40px;
+    width: 90px;
+    height: 10px;
+    background-color: #000;
+}
+</style>
